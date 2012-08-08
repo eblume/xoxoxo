c9b10e4af364c7a4076f50ca0a6b50e3c955596f
Erich Blume <blume.erich@gmail.com>
2012-08-08 10:29:53 -0700

    More basic structuring for JS

--- public/js/xoxoxo.js
+++ public/js/xoxoxo.js
@@ -1,20 +1,75 @@
 
 
-
 /**
  * Create an instance of a Game, which tracks a series of tictactoe matches.
  *
  * @constructor
  * @this {Game}
+ * @param {Element} tabe The 3x3 table that makes the board.
  */
-function Game() {
-  
+function Game(table) {
+  this.board = Board(table);
+  this.players = [HumanPlayer(),AIPlayer()];
+  board.init();
+}
+
+/**
+ * Start a new match, requiring that any previous match must have ended.
+ *
+ * This will automatically call this.clearmatch(), so make sure you score
+ * any previous matches first!
+ *
+ * @this {Game}
+ */
+Game.prototype.startmatch() {
+  throw new Error("Not yet implemented");
+
+  // Make sure that the board is ready for a new match.
+  this.clearmatch();
+}
+
+/**
+ * Clear out any existing match (without scoring) to prep for a new one.
+ *
+ * @this {Game}
+ */
+Game.prototype.clearmatch() {
+  throw new Error("Not yet implemented");
+}
+
+/**
+ * Create an instance of a Board, which tracks the board state and provides basic logic.
+ *
+ * The Board class model is designed to be highly 'functional'. As a result, all manipulation
+ * methods will NOT modify the current board, but will instead return a new Board object that
+ * reflect the new state. This will help in many areas, not least of which being recursive game
+ * tree searches.
+ * 
+ * @this {Board}
+ * @param {Element} table The 3x3 table which makes the board.
+ */
+function Board(table) {
+  throw new Error("Not yet implemented");
 }
 
 
+/**
+ * Create a new HumanPlayer object.
+ *
+ * @this {HumanPlayer}
+ */
+function HumanPlayer() {
+  throw new Error("Not yet implemented");
+}
 
-
-
+/**
+ * Create new AIPlayer object.
+ *
+ * @this {AIPlayer}
+ */
+function AIPlayer() {
+  throw new Error("Not yet implemented");
+}
 
 
 /** 
@@ -23,9 +78,8 @@ function Game() {
  * @param {Element} table The table element, a 3x3 grid, used as a board.
  */
 function tictactoe(table) {
-
-  var game = Game()
-
+  var game = Game(table)
+  game.startmatch();
 }
 
 
---
+++ public/js/xoxoxoai.js
@@ -0,0 +1,22 @@
+/* xoxoxoai.js
+ * by Erich Blume <blume.erich@gmail.com>
+ * 
+ * This script should be loaded before xoxoxo.js. It provides AI implementations,
+ * which all follow a standard convention of taking a board state and a player
+ * as input, and producing a new board state as output (such that the player given plays
+ * one move on the new board).
+ */
+
+
+/**
+ * Create a new gamestate where player has played on a random open tile.
+ *
+ * @param {Board} board The input game state, which will not be mutated.
+ * @param {AIPlayer} player The player that should be used to mark a new tile.
+ * @return {Board} The new (output) game state.
+ */
+
+function RandomAI(board,player) {
+  throw new Error("Not yet implemented.");
+
+}
