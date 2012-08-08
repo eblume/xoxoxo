6963a36a162ed5cc92ef1d6e43afb055734a1ce6
Erich Blume <blume.erich@gmail.com>
2012-08-08 08:16:37 -0700

    Slightly new JS scaffolding

--- public/js/xoxoxo.js
+++ public/js/xoxoxo.js
@@ -1,27 +1,32 @@
-/* makeClickHandler(index, element)
- *
- * Assign the proper click handler to each game cell.
- *
- * index - not used here, ignore it. Instead use "elem.id" for unique IDs.
- * elem - the element that is to be assigned a click handler.
- */
-var makeClickHandler = function(index,elem) {
 
+
+
+/**
+ * Create an instance of a Game, which tracks a series of tictactoe matches.
+ *
+ * @constructor
+ * @this {Game}
+ */
+function Game() {
+  
 }
 
 
 
 
-/* Document Ready handler
- *
- * Attaches event handlers and/or sets initial timeouts for unobtrusive JS.
+
+
+
+/** 
+ * Given a 3x3 table jQuery element, initializes a game of tic tac toe.
+ * 
+ * @param {Element} table The table element, a 3x3 grid, used as a board.
  */
-$(function() {
-  $("#board td").each(makeClickHandler);
-});
+function tictactoe(table) {
 
+  var game = Game()
 
-
+}
 
 
 
